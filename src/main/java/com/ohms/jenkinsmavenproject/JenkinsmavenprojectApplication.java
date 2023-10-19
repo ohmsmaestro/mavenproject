6388b9326672c24a7fd76a3f6c118e56b45ceb72
Omenesa Muhammed <muhammed.omenesa@microbridge.com.ng>
2023-10-19 16:13:01 +0100

    init

---
+++ src/main/java/com/ohms/jenkinsmavenproject/JenkinsmavenprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ohms.jenkinsmavenproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsmavenprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsmavenprojectApplication.class, args);
+    }
+
+}
